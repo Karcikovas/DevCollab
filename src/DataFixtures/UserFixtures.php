7e945f5138d6531dbf6bc96298f04242f0b49218
“karolis <karolis.petrikas@mail.com>
2019-11-02 20:54:03 +0000

    Accessing Services from the Fixtures added admin/admin

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,10 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\User\User;
 
 class UserFixtures extends Fixture
 {
@@ -18,12 +18,15 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new User();
+        $user->setUsername('admin');
         // $product = new Product();
         // $manager->persist($product);
+        $password = $this->encoder->encodePassword($user, 'admin');
         $user->setPassword($this->passwordEncoder->encodePassword(
             $user,
             'the_new_password'
         ));
+        $manager->persist($user);
         $manager->flush();
     }
 }
